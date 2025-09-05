ee665459669cfb85d180a2709ba729327bdd7a5a
gamedev <>
2025-09-05 11:58:35 +0200

    KAN 117 Start the knoback feature

---
+++ BluntBrawl/Assets/_/Feature/Colision/Runtime/PlayerColisionFeedback.cs
@@ -0,0 +1,37 @@
+using Health.Runtime;
+using Interfaces.Runtime;
+using UnityEngine;
+
+namespace Colision.Runtime
+{
+    [RequireComponent(typeof(HealthBehaviour))]
+    public class PlayerColisionFeedback : MonoBehaviour, IBumpable
+    {
+        #region Publics Varia
+        
+
+        
+
+        #endregion
+        
+        #region Utils
+        
+        
+        public void PlayerBumpOnHit(Vector3 direction, float force)
+        {
+            if (HealthBehaviour.)
+        }
+        
+        #endregion
+
+
+        #region Privates
+
+        
+        [SerializeField] private Rigidbody _playerRigidbody;
+        private HealthBehaviour _playerHealth => GetComponent<HealthBehaviour>();
+        
+
+        #endregion
+    }
+}

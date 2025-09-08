6482f64a9081166ee3bc4ce6cd66611aab614797
solene <solenerise@gmail.com>
2025-09-08 14:58:15 +0200

    KAN 000 Fix from previous merge.

--- BluntBrawl/Assets/_/Feature/Stats/Runtime/WeaponStats.cs
+++ BluntBrawl/Assets/_/Feature/Stats/Runtime/WeaponStats.cs
@@ -7,6 +7,7 @@ public class WeaponStats : ScriptableObject
     public int m_damage;
     public float m_velocityRequired;
     public int m_velocityDamageMultiplier;
+    public float m_invincibilityDuration;
     public float m_force;
 
     public GameObject m_inWorldPrefab;
